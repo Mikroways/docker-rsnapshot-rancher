09e79b66fd05cf1ddccd039f61a41f3a6fadc65e
Christian Rodriguez <chrodriguez@gmail.com>
2017-01-03 15:21:24 -0300

    add only one retantion

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,20 +5,12 @@ set -e
 
 IFS=$'\n'
 
-[ -z "${PREFIX}" ] || PREFIX=`echo "${PREFIX}_" | sed 's@[-. ]@_@g'`
+[ -z "${PREFIX}" ] || mkdir -p /var/rsnapshot/${PREFIX}
 
-envsubst '${RSNAPSHOT_HOURLY_RETAIN}${RSNAPSHOT_DAILY_RETAIN}${RSNAPSHOT_WEEKLY_RETAIN}${$RSNAPSHOT_MONTHLY_RETAIN}${RSNAPSHOT_YEARLY_RETAIN}${PREFIX}' < /etc/rsnapshot.conf.tpl > /etc/rsnapshot.conf
+envsubst '${RETAIN}${PREFIX}' < /etc/rsnapshot.conf.tpl > /etc/rsnapshot.conf
 for backup in `echo -e $BACKUP_DIRECTORIES`; do
   AUX=`echo $backup | sed -r  "s@(^[^ ]+) +@\1\t@g"`
   echo -e "backup\t$AUX" >> /etc/rsnapshot.conf
 done
 
-if [ $# -le 1 ]; then
-  if [ -z "$1" -o $1 = "configtest" ]; then
-    exec rsnapshot "$@"
-  else
-    exec rsnapshot $PREFIX$1
-  fi
-else
-  exec "$@"
-fi
+exec rsnapshot "$@"
--- rancher/docker-entrypoint-rancher.sh
+++ rancher/docker-entrypoint-rancher.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-PREFIX=$(curl http://rancher-metadata.rancher.internal/2015-12-19/self/service/stack_name)_$(curl http://rancher-metadata.rancher.internal/2015-12-19/self/service/name)
+PREFIX=$(curl http://rancher-metadata.rancher.internal/2015-12-19/self/service/stack_name)/$(curl http://rancher-metadata.rancher.internal/2015-12-19/self/service/name)
 
 [ -z "$PREFIX" ] && (echo "PREFIX must be set to run this container, but can't get its stack and service name from rancher metadata"; exit 1)
 
